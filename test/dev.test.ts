7d30a2426b1678d9d517ef6fe967e9ce68d518d1
unadlib <unadlib@gmail.com>
2023-01-19 05:32:13 +0800

    perf(dev): add dev mode testing

---
+++ test/dev.test.ts
@@ -0,0 +1,87 @@
+import { create } from '../src';
+
+test('custom shallow copy without checking in prod mode', () => {
+  global.__DEV__ = false;
+  const baseState = { foo: { bar: 'test' } };
+
+  expect(() => {
+    create(
+      baseState,
+      (draft) => {
+        draft.foo.bar = 'test2';
+      },
+      {
+        enableAutoFreeze: true,
+        // @ts-expect-error
+        mark: (target) => {
+          if (target === baseState.foo) {
+            return () => ({ ...target });
+          }
+        },
+      }
+    );
+  }).not.toThrowError();
+
+  expect(() => {
+    create(
+      baseState,
+      (draft) => {
+        draft.foo.bar = 'test2';
+      },
+      {
+        enablePatches: true,
+        // @ts-expect-error
+        mark: (target) => {
+          if (target === baseState.foo) {
+            return () => ({ ...target });
+          }
+        },
+      }
+    );
+  }).not.toThrowError();
+});
+
+test('custom shallow copy with checking in dev mode', () => {
+  global.__DEV__ = true;
+  const baseState = { foo: { bar: 'test' } };
+
+  expect(() => {
+    create(
+      baseState,
+      (draft) => {
+        draft.foo.bar = 'test2';
+      },
+      {
+        enableAutoFreeze: true,
+        // @ts-expect-error
+        mark: (target) => {
+          if (target === baseState.foo) {
+            return () => ({ ...target });
+          }
+        },
+      }
+    );
+  }).toThrowErrorMatchingInlineSnapshot(
+    `"You can't use mark and patches or auto freeze together."`
+  );
+
+  expect(() => {
+    create(
+      baseState,
+      (draft) => {
+        draft.foo.bar = 'test2';
+      },
+      {
+        enablePatches: true,
+        // @ts-expect-error
+        mark: (target) => {
+          if (target === baseState.foo) {
+            return () => ({ ...target });
+          }
+        },
+      }
+    );
+  }).toThrowErrorMatchingInlineSnapshot(
+    `"You can't use mark and patches or auto freeze together."`
+  );
+});
