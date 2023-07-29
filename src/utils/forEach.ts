b1a606cd9fd2aa33b36cbcfcd5db192c4ffd5ab0
unadlib <unadlib@gmail.com>
2023-07-30 01:03:48 +0800

    refactor(currying): refactor forEach

--- src/utils/forEach.ts
+++ src/utils/forEach.ts
@@ -3,26 +3,21 @@ import { getType } from './draft';
 
 export function forEach<T extends object>(
   target: T,
-  iter: (key: string | number | symbol, value: any, source: T) => void,
-  enumerableOnly = false
+  iter: (key: string | number | symbol, value: any, source: T) => void
 ) {
   if (getType(target) === DraftType.Object) {
-    const getKeys = enumerableOnly ? Object.keys : Reflect.ownKeys;
-    getKeys(target).forEach((key) => {
-      if (!enumerableOnly || typeof key !== 'symbol')
-        iter(key, (target as any)[key], target);
+    Reflect.ownKeys(target).forEach((key) => {
+      iter(key, (target as any)[key], target);
     });
-    return;
-  }
-  if (getType(target) === DraftType.Array) {
+  } else if (getType(target) === DraftType.Array) {
     let index = 0;
     for (const entry of target as any[]) {
       iter(index, entry, target);
       index += 1;
     }
-    return;
+  } else {
+    (target as Map<any, any> | Set<any>).forEach((entry: any, index: any) =>
+      iter(index, entry, target)
+    );
   }
-  (target as Map<any, any> | Set<any>).forEach((entry: any, index: any) =>
-    iter(index, entry, target)
-  );
 }
