9b1c2a86519f6a84e63e474d8063798ece17f8c3
unadlib <unadlib@gmail.com>
2023-08-02 02:49:36 +0800

    test(api): add makeCreator() test

--- test/makeCreator.test.ts
+++ test/makeCreator.test.ts
@@ -34,3 +34,126 @@ test('check enableAutoFreeze', () => {
   });
   expect(Object.isFrozen(state)).toBeTruthy();
 });
+
+test('check enableAutoFreeze - override', () => {
+  const create = makeCreator({
+    enableAutoFreeze: true,
+  });
+  const baseState = { foo: { bar: 'str' }, arr: [] };
+  const state = create(
+    baseState,
+    (draft) => {
+      //
+    },
+    {
+      enableAutoFreeze: false,
+    }
+  );
+  expect(Object.isFrozen(state)).not.toBeTruthy();
+});
+
+test('check enablePatches', () => {
+  const create = makeCreator({
+    enablePatches: true,
+  });
+  const baseState = { foo: { bar: 'str' }, arr: [] };
+  const state = create(baseState, (draft) => {
+    //
+  });
+  expect(Array.isArray(state)).toBeTruthy();
+});
+
+test('check enablePatches - override', () => {
+  const create = makeCreator({
+    enablePatches: true,
+  });
+  const baseState = { foo: { bar: 'str' }, arr: [] };
+  const state = create(
+    baseState,
+    (draft) => {
+      //
+    },
+    {
+      enablePatches: false,
+    }
+  );
+  expect(Array.isArray(state)).not.toBeTruthy();
+});
+
+test('check strict', () => {
+  const create = makeCreator({
+    strict: true,
+  });
+  class C {
+    x = 0;
+  }
+  const baseState = { foo: { bar: 'str' }, arr: [], c: new C() };
+  expect(() => {
+    const state = create(baseState, (draft) => {
+      draft.c.x = 1;
+    });
+  }).toThrowError();
+});
+
+test('check strict - override', () => {
+  const create = makeCreator({
+    strict: true,
+  });
+  class C {
+    x = 0;
+  }
+  const baseState = { foo: { bar: 'str' }, arr: [], c: new C() };
+  expect(() => {
+    const state = create(
+      baseState,
+      (draft) => {
+        draft.c.x = 1;
+      },
+      { strict: false }
+    );
+  }).not.toThrowError();
+});
+
+test('check mark', () => {
+  class C {
+    x = 0;
+  }
+  const create = makeCreator({
+    mark: (target, { immutable }) => {
+      if (target instanceof C) {
+        return immutable;
+      }
+    },
+  });
+  const baseState = { foo: { bar: 'str' }, arr: [], c: new C() };
+  const state = create(baseState, (draft) => {
+    draft.c.x = 1;
+  });
+  expect(state.c.x).toBe(1);
+  expect(state.c).not.toBe(baseState.c);
+});
+
+test('check mark - override', () => {
+  class C {
+    x = 0;
+  }
+  const create = makeCreator({
+    mark: (target, { immutable }) => {
+      if (target instanceof C) {
+        return immutable;
+      }
+    },
+  });
+  const baseState = { foo: { bar: 'str' }, arr: [], c: new C() };
+  const state = create(
+    baseState,
+    (draft) => {
+      draft.c.x = 1;
+    },
+    {
+      mark: undefined,
+    }
+  );
+  expect(state.c.x).toBe(1);
+  expect(state.c).toBe(baseState.c);
+});
