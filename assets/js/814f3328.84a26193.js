6da98ac2f612b7bf340ee5e471db2c47999accb0
unadlib <unadlib@gmail.com>
2023-12-16 04:08:25 +0800

    Deploy website - based on 8a61821542c70ed99ebb52f30e8c9c855b9b6df7

---
+++ assets/js/814f3328.84a26193.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Announcing Mutative 1.0 - A New Era in Efficient Immutable Updates","permalink":"/blog/releases/1.0","unlisted":false}]}')}}]);
