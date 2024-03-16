48f5efb5452ada21f4614442c42defad470c9343
unadlib <unadlib@gmail.com>
2024-03-16 15:37:58 +0800

    docs(website): update img

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/mutative-social-card.jpg',
       docs: {
         sidebar: {
           hideable: true,
